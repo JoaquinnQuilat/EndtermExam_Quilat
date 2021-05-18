601f269eb2e597eef380b64e54bab1bc3fa0b6b8
Joaquinn B. Quilat <79894232+JoaquinnQuilat@users.noreply.github.com>
2021-05-18 14:54:30 +0800

    Add files via upload

--- src/AccountUnitTest.java
+++ src/AccountUnitTest.java
@@ -4,10 +4,22 @@ public class AccountUnitTest
 {
     public static void main(String[] args)
     {
-        Checking aCheckingAccount = new Checking(1);
-        Savings aSavingsAccount = new Savings(2, 1.6);
+        Account[] accountArray = new Account[10];
 
-        JOptionPane.showMessageDialog(null, aCheckingAccount.getAccountInfo());
-        JOptionPane.showMessageDialog(null, aSavingsAccount.getAccountInfo());
+        accountArray[0] = new Checking(1);
+        accountArray[1] = new Checking(2);
+        accountArray[2] = new Checking(3);
+        accountArray[3] = new Checking(4);
+        accountArray[4] = new Checking(5);
+        accountArray[5] = new Savings(11, 1.0);
+        accountArray[6] = new Savings(12, 1.1);
+        accountArray[7] = new Savings(23, 1.2);
+        accountArray[8] = new Savings(34, 1.3);
+        accountArray[9] = new Savings(35, 1.4);
+
+
+        for (Account account : accountArray) JOptionPane.showMessageDialog(null, account.getAccountInfo());
+
+
     }
 }
